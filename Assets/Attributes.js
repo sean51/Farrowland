0198b0850ebc31e0416fb45af0e1786344e86c16
artknick <artknick@gmail.com>
2015-06-17 01:14:10 -0700

    Attributes and Hero and Monster Class
    Created 3 classes

---
+++ Assets/Attributes.js
@@ -0,0 +1,47 @@
+﻿#pragma strict
+public class Attributes extends MonoBehaviour{
+	var health : int;
+	var armor : int;
+	var speed : int;
+	var damage : int;
+	var magicDamage : int;
+	var isAlive : boolean;
+	// lets you know if the character is alive.
+	protected function boolean getAlive(){
+		return isAlive;
+	}
+	protected function int getHealth(){
+		return health;
+	}
+	protected function int getArmor() {
+		return armor;
+	}
+	protected function int getSpeed() {
+		return speed;
+	}
+	protected function int getDamage() {
+		return damage;
+	}
+	protected function int getMagicDamage() {
+		return magicDamage;
+	}
+	// set the Character to dead.
+	protected function boolean setAlive(theIsAlive : boolean) {
+		return isAlive = theIsAlive;
+	}
+	protected function int setHealth(theHealth : int) {
+		return health = theHealth;
+	}
+	protected function int setArmor(theArmor : int) {
+		return armor = theArmor;
+	}
+	protected function int setSpeed(theSpeed : int) {
+		return speed = theSpeed;
+	}
+	protected function int setDamage(theDamage : int) {
+		return damage = theDamage;
+	}
+	protected function int setMagicDamage(theMagicDamage : int) {
+		return magicDamage = theMagicDamage;
+	}
+}
---
+++ Assets/Hero.js
@@ -0,0 +1,15 @@
+﻿#pragma strict 
+public class Hero extends Attributes {
+	var health : setHealth(60);
+	var armor : setArmor(5);
+	var speed : setSpeed(2);
+	var damage : setDamage(5);
+	var magicDamage : setMagicDamage(5);
+	var isAlive : true;
+}
+function TakeDamage(theDamageTaken : int) { 
+	setHealth( getHealth() - theDamageTaken);
+}
+function int DealDamage() { 
+	GameObject.GetComponent<Monster>().SendMessage("TakeDamage", getDamage());
+}
