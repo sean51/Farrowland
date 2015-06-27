ccf86b52888ed9fd71a46d2ee56c0d3bd89d10fa
artknick <artknick@gmail.com>
2015-06-27 00:12:53 -0700

    Quest and Package
    Nothing yet

---
+++ Assets/Package.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+public class Package
+{
+	public var test_for_quest : String;
+	public var test_area : Area = new Area();
+	function Package()
+	{
+		test_for_quest = "Hello i am a quest";
+		test_area = new Area(1);
+	}
+}
---
+++ Assets/Quest.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+public class Quest extends Area
+{
+
+	public var is_complete : boolean;
+	public var test_area : Area = new Area();
+	
+	function Quest(quest_pack : Package)
+	{
+		var quest2_string : String = quest_pack.test_for_quest;
+		var quest2_area : Area = quest_pack.test_area;
+	}
+	function Quest(completion : boolean, quest_taken : boolean)
+	{
+		is_complete = completion;
+		has_quest = quest_taken;
+	}
+
+}
